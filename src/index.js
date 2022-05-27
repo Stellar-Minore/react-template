6f33a71c6373494c453373356c026c125fe63bb1
SamrahS <samrah@stellarminore.com>
2022-05-27 15:40:30 +0500

    Update project index to use react 18 format

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './components/app';
+import { App } from './components/app';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<Router>
-			<App />
-		</Router>
-	</React.StrictMode>,
-	document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+	<Router>
+		<App />
+	</Router>
 );
 
 reportWebVitals();
