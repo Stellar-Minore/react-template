7a5076c9e79f883f2f66951f2853c4dc1636605e
farhad0724 <fakhter@stellarminore.com>
2024-08-19 13:43:14 +0500

    Set up routing in app router component

--- src/components/appRouter/appRouter.jsx
+++ src/components/appRouter/appRouter.jsx
@@ -1,7 +1,26 @@
-function AppRouter({ component: Component, ...props }) {
+import { Routes, Route } from 'react-router-dom';
+
+export const AppRouter = ({ routes }) => {
+	return (
+		<Routes>
+			{routes.map(({
+				path, title, component
+			}) => (
+				<Route
+					key={path}
+					path={path}
+					element={
+						<RouteComponent
+							title={title}
+							component={component} />
+					} />
+			))}
+		</Routes>
+	);
+};
+
+const RouteComponent = ({ component: Component, ...props }) => {
 	document.title = props.title;
 
-	return <Component {...props} />;
-}
-
-export { AppRouter };
+	return <Component />;
+};
